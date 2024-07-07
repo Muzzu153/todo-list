a4a3a13761fee674432ee4ceb587446f52460c37
Muzaffer <muzzusyed153@gmail.com>
2024-07-07 14:21:55 +0530

    Set up the project with webpacks

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/main.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+
+      {
+        test: /\.(jpg|png|svg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "./images/[name][ext]",
+        },
+      },
+      {
+        test: /\.(woff|woff2)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "./fonts/[name][ext]",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "ToDoList",
+      filename: "index.html",
+      template: "./src/index.html",
+      eject: "body",
+    }),
+  ],
+};
