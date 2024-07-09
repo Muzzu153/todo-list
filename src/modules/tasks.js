255bdb3ff911d0078b7eaa0fbe6653f2131590e1
Muzaffer <muzzusyed153@gmail.com>
2024-07-09 14:41:28 +0530

    Made a module which create individual tasks

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -1,9 +1,4 @@
-const Tasks = () => {
-  const allTasks = [];
-
-  const getTask = () => allTasks;
-
-  const getTaskValuesFromUser = () => {
+  const createTask = () => {
     const title = prompt("title?");
     const description = prompt("description?");
     const dueDate = prompt("due-date?");
@@ -12,11 +7,4 @@ const Tasks = () => {
     return { title, description, dueDate, priority };
   };
 
-  const addTask = (task) => {
-    allTasks.push(task);
-  };
-
-  return { getTask, getTaskValuesFromUser, addTask };
-};
-
-export default Tasks;
+export default createTask;
